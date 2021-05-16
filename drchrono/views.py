069b672feba175e1a495f9265a26329d7afacffd
Tom S <tom_wrk@outlook.com>
2021-05-16 16:58:52 -0700

    Added "Patients List"

--- drchrono/views.py
+++ drchrono/views.py
@@ -206,3 +206,89 @@ def appointments_create(request):
             "colors": colors,
         },
     )
+
+
+def patients_list(request):
+    form = AppointmentsForm(request.POST or None)
+    doctor_welcome = DoctorWelcome()
+    api = Api(doctor_welcome.get_token())
+    doctor = doctor_welcome.make_api_request()
+    office = api.offices_read({"doctor": doctor["id"]})
+    exam_rooms = office["exam_rooms"]
+    # Updating schedule
+    ####################
+    # if bool(form.data.get("update")):
+    #     api.appointments_update(
+    #         id=form.data.get("id"),
+    #         data={
+    #             "scheduled_time": form.data.get("s_date")
+    #             + "T"
+    #             + form.data.get("s_time")
+    #             + ":00",
+    #             "duration": form.data.get("duration"),
+    #             "patient": form.data.get("patient"),
+    #             "notes": form.data.get("notes"),
+    #             "reason": form.data.get("reason"),
+    #             "status": form.data.get("status"),
+    #             "exam_room": form.data.get("exam_room"),
+    #             "billing_status": form.data.get("billing_status"),
+    #             "color": form.data.get("color"),
+    #         },
+    #     )
+    # Deleting schedule
+    ####################
+    # if bool(form.data.get("delete")):
+    #     api.appointments_update(id=form.data.get("id"), data={"deleted_flag": True})
+    # Date range/start & end
+    #########################
+    # if bool(form.data.get("update_range")):
+    #     start = str(form.data.get("start"))
+    #     end = form.data.get("end")
+    #     dt_start = datetime.datetime.strptime(start, "%Y-%m-%d")
+    #     dt_end = datetime.datetime.strptime(end, "%Y-%m-%d")
+    # else:
+    #     dt_now = datetime.datetime.now()
+    #     dt_start = dt_now + datetime.timedelta(days=-30)
+    #     dt_end = dt_now + datetime.timedelta(days=30)
+    #     start = dt_start.strftime("%Y-%m-%d")
+    #     end = dt_end.strftime("%Y-%m-%d")
+    # working_days = np.busday_count(dt_start.date(), dt_end.date())
+    # Appointments & Patients
+    ##########################
+    # appointments = api.appointments_list(
+    #     start=start, end=end, params={"deleted_flag": False}
+    # )
+    patients = api.patients_list(params={"doctor": doctor["id"]})
+
+    for p in patients:
+        print(p)
+        print("")
+
+    # duration, date & time
+    ########################
+    # duration = 0
+    # for k, a in enumerate(appointments):
+    #     s = str(a["scheduled_time"]).split("T")
+    #     appointments[k]["s_date"] = s[0]
+    #     appointments[k]["s_time"] = s[1][:-3]
+    #     color = str(a["color"])
+    #     color = color.lower()
+    #     color = color.replace(" ", "")
+    #     appointments[k]["color"] = color
+    #     duration += a["duration"]
+    # waiting = working_days * 8 * 60 - duration
+    # Return
+    ##########
+    return render(
+        request,
+        "patients_list.html",
+        {
+            "form": form,
+            "title": "My Form 123",
+            "doctor": doctor,
+            "patients": patients,
+            "exam_rooms": exam_rooms,
+            "statuses": statuses,
+            "colors": colors,
+        },
+    )
